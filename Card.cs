f3370e9afd3b10c475d0e90cc12a55dc7ac94e39
danpung2 <danpung_2@naver.com>
2023-07-06 18:22:13 +0900

    ADD: Card Flip

--- Card.cs
+++ Card.cs
@@ -2,12 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Card : MonoBehaviour
 {
+    [SerializeField] private SpriteRenderer cardRenderer;
+    [SerializeField] private Sprite animalSprite;
+    [SerializeField] private Sprite backSprite;
+
+    private bool _isFlipped;
+    
     void Start()
     {
-        
+        _isFlipped = false;
     }
 
     void Update()
@@ -15,4 +22,32 @@ public class Card : MonoBehaviour
         
     }
 
+    public void FlipCard()
+    {
+        Vector3 originalScale = transform.localScale;
+        Vector3 targetScale = new Vector3(0f, originalScale.y, originalScale.z);
+
+        transform.DOScale(targetScale, 0.2f).OnComplete(() =>
+        {
+            _isFlipped = !_isFlipped;
+        
+            if (_isFlipped)
+            {
+                cardRenderer.sprite = animalSprite;
+            }
+            else
+            {
+                cardRenderer.sprite = backSprite;
+            }
+
+            transform.DOScale(originalScale, 0.2f);
+        });
+
+    }
+    
+    void OnMouseDown()
+    {
+        FlipCard();    
+    }
+    
 }
