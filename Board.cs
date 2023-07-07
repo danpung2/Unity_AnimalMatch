fc1bdc5252e7f67788b9288de3cc59afdf9cbe5f
danpung2 <danpung_2@naver.com>
2023-07-07 15:47:02 +0900

    ADD: card shuffle

--- Board.cs
+++ Board.cs
@@ -7,11 +7,12 @@ public class Board : MonoBehaviour
     [SerializeField] private GameObject cardPrefab;
     [SerializeField] private Sprite[] cardSprites;
 
-    private List<int> cardIdList = new List<int>();
+    private List<int> _cardIdList = new List<int>();
 
     void Start()
     {
         GenerateCardId();
+        ShuffleCardId();
         InitBoard();
     }
 
@@ -19,8 +20,18 @@ public class Board : MonoBehaviour
     {
         for (int i = 0; i < cardSprites.Length; i++)
         {
-            cardIdList.Add(i);
-            cardIdList.Add(i);
+            _cardIdList.Add(i);
+            _cardIdList.Add(i);
+        }
+    }
+
+    void ShuffleCardId()
+    {
+        int cardCount = _cardIdList.Count;
+        for (int i = 0; i < cardCount; i++)
+        {
+            int randomIndex = Random.Range(i, cardCount);
+            (_cardIdList[randomIndex], _cardIdList[i]) = (_cardIdList[i], _cardIdList[randomIndex]);
         }
     }
 
@@ -45,9 +56,9 @@ public class Board : MonoBehaviour
                 GameObject cardObject = Instantiate(cardPrefab, pos, Quaternion.identity);
                 
                 Card card = cardObject.GetComponent<Card>();
-                int cardId = cardIdList[cardIndex++];
+                int cardId = _cardIdList[cardIndex++];
                 
-                card.SetCardId(cardIdList[cardId]);
+                card.SetCardId(_cardIdList[cardId]);
                 card.SetAnimalSprite(cardSprites[cardId]);
 
             }
